aa652b5144a7a22b4ff696b732928bd31a6cde49
Data-Science-User <Data-Science-User@users.noreply.github.com>
2016-08-18 19:23:49 -0400

    Update Coding_Challenge_Q1.R

--- Coding_Challenge_Q1.R
+++ Coding_Challenge_Q1.R
@@ -1,32 +1,10 @@
-
-
-# 1 2	3
-# 4	5	6
-# 7	8	9
-#   0	
-
-
 # The knight makes TT jumps to other keys according to its allowable moves 
 # (so that from each key it has two or three allowable moves). 
 # The knight chooses amongst the allowable moves uniformly at random and 
 # keeps track of the running sum SS of keys on which it lands. 
 # We are interested in SS under the modulo operator.
 
-# Start at 0
-# If any number 0-9,then conditionally and randomly sample from a subset from 0-9
-# After each move, take the S sum of numbers, and record the amount of T moves
-# Calculate modulotr operator by S mod T (remainder from S/T)
-
-# 0, then 4 or 6
-# 1, then 6 or 8
-# 2, then 7 or 9
-# 3, then 4 or 8
-# 4, then 3 or 9 or 0
-# 6, then 1 or 7 or 0
-# 7, then 2 or 6
-# 8, then 1 or 3
-# 9, then 2 or 4
-
+# Function that simulates the problem
 T = list() # Counter of the number of moves
 list_keys = list() # list of keys landed
 List_of_Moves = list("0" = list(4,6), "1" = list(6,8), "2" = list(7,9), 
@@ -53,8 +31,13 @@ p <- c(0.082710, 0.111331, 0.093211, 0.110218, 0.085769, 0.117118,
        0.085703, 0.109429, 0.092786,0.111725)
 
 weighted.mean(y,p) # 4.585107
+
+After 10 moves, the expected value is 4.585107
+
 # Q2: After T=10 moves, what is the standard deviation of the quantity of S mod 10?
 sd(x) # 2.861564
+
+After 10 moves, the standard deviation is 2.861564
 
 # Q3: After T=1024 moves, what is the expected value of the quantity of S mod 1024?
 options(expressions = 500000)
@@ -63,8 +46,13 @@ x_1024 = replicate(100000, {sum(sim(init = "0", size = 1024)) %% 1024})
 tab_x_1024 = prop.table(table(x_1024))
 
 weighted.mean(as.numeric(names(tab_x_1024)), (tab_x_1024/sum(x))) # 511.264
+
+After 1024 moves, the expected value is 511.264
+
 # Q4: After T=1024 moves, what is the standard deviation of the quantity of S mod 1024?
 sd(x_1024) # 70.21152
+
+After 1024 moves, the standard deviation is 70.21152
 
 # Q5: After T=10 moves, what is the probability that the sum S is divisible by 5, 
 # given that it is divisible by 7.
@@ -76,7 +64,10 @@ db7 <- x_cond_prob_1 %% 7 == 0
 meh<- table(db5, db7) / length(x_cond_prob_1)
 
 sum(db5 & db7) / sum(db7) # 0.1813455
-# After T=1024 moves, what is the probability that the sum S is divisible by 23, 
+
+After 10 moves, there is a 18% probability that the sum is divisible by 5.
+
+# Q6:  After T=1024 moves, what is the probability that the sum S is divisible by 23, 
 # given that it is divisible by 29.
 x_cond_prob_2 <- replicate(100000, sum(sim(init = "0", size = 1024)))
 
@@ -84,5 +75,7 @@ db23 <- x_cond_prob_2 %% 23 == 0
 db29 <- x_cond_prob_2 %% 29 == 0
 
 sum(db23 & db29) / sum(db29) # 0.1207658
+
+After 1024 moves, there is a 12% probability that the sum is divisible by 23.
 
 
